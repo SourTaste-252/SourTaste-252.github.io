436dea345439b822134a76745547e07bcf26b4f0
SourTaste <lawrenceatie53@gmail.com>
2025-11-05 15:31:14 +0800

    Update FITB.js

--- js/FITB.js
+++ js/FITB.js
@@ -59,7 +59,7 @@ trayToggle.addEventListener('click', () => {
     function loadQuiz(quizName, folderDisplayName) {
         quizTitle.textContent = folderDisplayName;
 
-        fetch(`../../js/Quizes_FITB/${quizName}.json`)
+        fetch(`/SourTaste-252/js/Quizes_FITB/${quizName}.json`)
             .then(res => res.json())
             .then(quiz => {
                 if (!quiz) {
@@ -238,4 +238,5 @@ trayToggle.addEventListener('click', () => {
     // ✅ Run once on page load
 
     updateFolderScores();
+
 
